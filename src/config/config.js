abe7d0606557d32dd4b6731765aaa17f4b83ba2f
daisycrego <daisycrego@gmail.com>
2021-05-18 22:24:26 -0400

    Update config with a different database name.

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,15 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
+  mongoUri:
+    process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
-}
+    "mongodb://" +
+      (process.env.IP || "localhost") +
+      ":" +
+      (process.env.MONGO_PORT || "27017") +
+      "/jbg-admin",
+};
 
-export default config
+export default config;
