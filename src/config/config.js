efb211bb53c332568c2b4a53a709edb2f82b7ea1
daisycrego <daisycrego@gmail.com>
2021-05-18 22:31:39 -0400

    Update gitignore and config with another database name to be consistent between cloud and local versions in case of a fallback.

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ const config = {
       (process.env.IP || "localhost") +
       ":" +
       (process.env.MONGO_PORT || "27017") +
-      "/jbg-admin",
+      "/jbgadmin",
 };
 
 export default config;
