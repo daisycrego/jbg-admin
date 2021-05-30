b451a509f97b739c701dde76374315f9764bbc1b
daisycrego <daisycrego@gmail.com>
2021-05-30 17:10:31 -0400

    Done integrating bull job queue worker to webpack build.

--- src/server/express.js
+++ src/server/express.js
@@ -24,9 +24,13 @@ import theme from "./../client/theme";
 import devBundle from "./devBundle";
 
 import { setupEventsWebhook } from "./webhooks";
+let Queue = require("bull");
 
 const CURRENT_WORKING_DIR = process.cwd();
 const app = express();
+
+let REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+let workQueue = new Queue("work", REDIS_URL);
 
 //comment out before building for production
 devBundle.compile(app);
@@ -42,6 +46,35 @@ app.use(helmet());
 app.use(cors());
 
 app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+// Kick off a new job by adding it to the work queue
+app.post("/job", async (req, res) => {
+  // This would be where you could pass arguments to the job
+  // Ex: workQueue.add({ url: 'https://www.heroku.com' })
+  // Docs: https://github.com/OptimalBits/bull/blob/develop/REFERENCE.md#queueadd
+  let job = await workQueue.add();
+  res.json({ id: job.id });
+});
+
+// Allows the client to query the state of a background job
+app.get("/job/:id", async (req, res) => {
+  let id = req.params.id;
+  let job = await workQueue.getJob(id);
+
+  if (job === null) {
+    res.status(404).end();
+  } else {
+    let state = await job.getState();
+    let progress = job._progress;
+    let reason = job.failedReason;
+    res.json({ id, state, progress, reason });
+  }
+});
+
+// You can listen to global events to get notified when jobs are processed
+workQueue.on("global:completed", (jobId, result) => {
+  console.log(`Job completed with result ${result}`);
+});
 
 // mount routes
 app.use("/", userRoutes);
