00c8b5153f3bb7d6007f485b1ff6e1b257890456
daisycrego <daisycrego@gmail.com>
2021-06-11 11:13:35 -0400

    Favicon attempt failed.

--- src/template.js
+++ src/template.js
@@ -8,7 +8,7 @@ export default ({ markup, css }) => {
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
           >
           <title>JBG Admin</title>
-          <link rel="shortcut icon" type="image/jpg" href="public/favicon.ico"/>
+          <link rel="shortcut icon" href="favicon.ico">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
           <style>
