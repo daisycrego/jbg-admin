ceb34e73e97dd2b7b03ace4eb0994c08a01e9771
daisycrego <daisycrego@gmail.com>
2021-08-19 12:47:35 -0400

    Refactored worker process 1) Removed a lot of comments, replaced with fewer useful comments 2) Shortened zillow flex alert emails, only including the most important links. 3) Renamed some vars.

--- src/worker.js
+++ src/worker.js
@@ -2,20 +2,17 @@ let throng = require("throng");
 let Queue = require("bull");
 let Event = require("./server/models/event.model");
 let Log = require("./server/models/log.model");
-const config = require("./config/config");
-const mongoose = require("mongoose");
 const { fubGET } = require("./fetch-fub");
 const nodemailer = require("nodemailer");
 const { connect } = require("./db");
 
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
+let smtpTransporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for 465, false for other ports
+  secure: false,
   auth: {
-    user: process.env.SMTP_USER, // generated ethereal user
-    pass: process.env.SMTP_PASS, // generated ethereal password
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
@@ -27,26 +24,26 @@ function isDateBeforeToday(date) {
 // connect to the database
 connect();
 
-// Connect to a local redis instance locally, and the Heroku-provided URL in production
+// Connect to redis instance
 let REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
 // Spin up multiple processes to handle jobs to take advantage of more CPU cores
-// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+// https://devcenter.heroku.com/articles/node-concurrency
 let workers = process.env.WEB_CONCURRENCY || 2;
 
-// The maximum number of jobs each worker should process at once. This will need
-// to be tuned for your application. If each job is mostly waiting on network
-// responses it can be much higher. If each job is CPU-intensive, it might need
-// to be much lower.
+// The maximum number of jobs each worker should process at once. Tuning: If each job is mostly waiting on network
+// responses it can be much higher. If each job is CPU-intensive, it might need to be much lower.
 let maxJobsPerWorker = 50;
 
 function start() {
-  // Connect to the named work queue
+  // Connect to the redis work queue
   let workQueue = new Queue("work", REDIS_URL);
 
+  // Worker - each job:
+  // 1.) Checks if the Event has already been processed. If yes, generates a Log and stops processing.
+  // 2.) Checks if the Event represents a possible Zillow Flex exemption. (Queries FUB API for the Person Data, checks if this is a New Lead from Zillow Flex). If yes, sends an alert email to the support admins.
+  // 3.) Creates an Event doc with all event data (eventId, etc) and processing data (isPossibleZillowExemption, etc.). Generates a Log of the processing outcome.
   workQueue.process(maxJobsPerWorker, async (job) => {
-    // This is an example job that just slowly reports on progress
-    // while doing no work. Replace this with your own job logic.
     if (!job.data || !job.data.uri) {
       console.log(`Missing job data or event URI.`);
       return;
@@ -84,13 +81,6 @@ function start() {
             leadCreatedDate = new Date(personData.created);
           }
 
-          // How to create a test date using the same date formatting
-          /*
-          const dateString = "2021-05-31T21:46:02+00:00".split("T")[0];
-          console.log(`dateString: ${dateString}`);
-          const testLeadCreatedDate = new Date(dateString);
-          */
-
           isNewLead = !isDateBeforeToday(leadCreatedDate);
           if (isNewLead) {
             console.log(`This is a new lead, created: ${leadCreatedDate}`);
@@ -117,37 +107,6 @@ function start() {
                 Zillow Property URL: ${eventData.property.url}
                 FUB URL: https://jillkbiggs.followupboss.com/2/people/view/${eventData.personId}
                 Zillow Premier Agent URL: ${personData.sourceUrl}
-
-                Event Details:
-                Event ID: ${event.eventId}
-                Resource IDs: ${event.resourceIds}
-                Event URI: ${event.resourceIds}
-                Event type: ${eventData.type}
-                Event source: ${eventData.source}
-                Property id: ${eventData.property.id}
-                Property street: ${eventData.property.street}
-                Property city: ${eventData.property.city}
-                Property state: ${eventData.property.state}
-                Property zipcode: ${eventData.property.code}
-                Property MLS: ${eventData.property.mlsNumber}
-                Property Price: ${eventData.property.price}
-                Property for Rent: ${eventData.property.forRent}
-                Property URL: ${eventData.property.url}
-
-                Person Details:
-                Person ID: ${personData.id}
-                Created: ${personData.created}
-                Updated: ${personData.updated}
-                Created via: ${personData.createdVia}
-                Last activity: ${personData.lastActivity}
-                Name: ${personData.name}
-                Emails: ${personData.emails}
-                Phones: ${personData.phones}
-                Stage: ${personData.stage}
-                Source: ${personData.source}
-                Source URL: ${personData.sourceUrl}
-                Price: ${personData.price}
-                Assigned to: ${personData.assignedTo}
                 View this lead on FUB: https://jillkbiggs.followupboss.com/2/people/view/${eventData.personId}
             `;
 
@@ -157,43 +116,13 @@ function start() {
                 <p> Zillow Property URL: ${eventData.property.url} </p>
                 <p> FUB URL: https://jillkbiggs.followupboss.com/2/people/view/${eventData.personId} </p>
                 <p> Zillow Premier Agent URL: ${personData.sourceUrl} </p>
-                <h3> Event Details </h3>
-                <ul>
-                    <li> Event ID: ${event.eventId} </li>
-                    <li> Event type: ${eventData.type} </li>
-                    <li> Event source: ${eventData.source} </li>
-                    <li> Property id: ${eventData.property.id} </li>
-                    <li> Property street: ${eventData.property.street} </li>
-                    <li> Property city: ${eventData.property.city} </li>
-                    <li> Property state: ${eventData.property.state} </li>
-                    <li> Property zipcode: ${eventData.property.code} </li>
-                    <li> Property MLS: ${eventData.property.mlsNumber} </li>
-                    <li> Property Price: ${eventData.property.price} </li>
-                    <li> Property for Rent: ${eventData.property.forRent} </li>
-                </ul>
-
-
-                <h3> Person Details </h3>
-                <ul>
-                <li> Person ID: ${personData.id} </li>
-                <li> Created: ${personData.created} </li>
-                <li> Updated: ${personData.updated} </li>
-                <li> Created via: ${personData.createdVia} </li>
-                <li> Last activity: ${personData.lastActivity} </li>
-                <li> Name: ${personData.name} </li>
-                <li> Stage: ${personData.stage} </li>
-                <li> Source: ${personData.source} </li>
-                <li> Source URL: ${personData.sourceUrl} </li>
-                <li> Price: ${personData.price} </li>
-                <li> Assigned to: ${personData.assignedTo} </li>
-                
-                </ul>
+                <p> <a href="https://jillkbiggs.followupboss.com/2/people/view/${eventData.personId}" target="_blank">View this lead on FUB</a> </p>
             </div>
             `;
 
             try {
               // send mail with defined transport object
-              let info = await transporter.sendMail({
+              let info = await smtpTransporter.sendMail({
                 from: '"cregodev7@gmail.com', // sender address
                 to: "daisycrego@gmail.com, cregodev7@gmail.com, dan@jillbiggsgroup.com, support@jillbiggsgroup.com", // list of receivers
                 subject: "Possible Zillow Flex", // Subject line
@@ -264,23 +193,21 @@ function start() {
           eventCreated: job.data.eventCreated,
           resourceIds: job.data.resourceIds,
           uri: job.data.uri,
-          errorMessage: `This event already exists in our DB, skipping it!`,
+          errorMessage: `This event already exists in our DB, skipping it`,
         });
         log.save();
       } catch (err) {
         console.log(err);
       }
-      console.log(`This event already exists in our DB, skipping it!`);
+      console.log(`This event already exists in our DB, skipping it`);
     }
     progress = 100;
     job.progress(progress);
 
-    // A job can return values that will be stored in Redis as JSON
-    // This return value is unused
     return { status: "Event processed successfully" };
   });
 }
 
 // Initialize the clustered worker process
-// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+// https://devcenter.heroku.com/articles/node-concurrency
 throng({ workers, start });
